93a92b8a1ee69703895cd993d2b56dcd2da6d968
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-05-18 17:41:46 +0300

    feat: connect to database

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
 
 const PORT = 3001;
+
+//connect to mongoDB
+main().catch((err) => console.log(err));
+async function main() {
+	await mongoose.connect('mongodb://localhost:27017/crud', () => {
+		console.log('Connected to Database');
+	});
+}
 
 app.listen(process.env.PORT || PORT, () => {
 	console.log(`Server Running on Port ${PORT}`);
