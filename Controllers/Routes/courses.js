7ad34788b9bb3516e67212a1cccfeab407a83407
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-05-19 12:21:52 +0300

    set up course route and save data to database

---
+++ Controllers/Routes/courses.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+const { Course } = require('../../Models/course');
+
+//Save course in db
+router.post('/', async (req, res) => {
+	if (!req.body.name || !req.body.numberOfHours)
+		return res.status(400).send('Name and Number of Hours are required');
+	let course = new Course({
+		name: req.body.name,
+		numberOfHours: req.body.numberOfHours,
+	});
+	course = await course.save(course);
+	res.send(course);
+});
+router.get('/', async (req, res) => {
+	res.send('Connected');
+});
+
+module.exports = router;
