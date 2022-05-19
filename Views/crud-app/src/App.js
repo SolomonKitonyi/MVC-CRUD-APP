1d8955b787b97a47a6261c527fecbd50fb2017df
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-05-19 15:41:38 +0300

    feat: fetch data from database and displayed it

--- Views/crud-app/src/App.js
+++ Views/crud-app/src/App.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import './App.css';
 
 function App() {
 	const [courseName, setCourseName] = useState('');
 	const [numberOfHours, setNumberOfHours] = useState(0);
+	const [courses, setCourses] = useState([]);
+
+	useEffect(() => {
+		Axios.get('http://localhost:3001/api/courses')
+			.then((response) => {
+				setCourses(response.data);
+			})
+			.catch((err) => console.log(err.message));
+	}, []);
 
 	const addCourse = () => {
 		Axios.post('http://localhost:3001/api/courses', {
@@ -20,6 +29,13 @@ function App() {
 			<label>Number of Hours: </label>
 			<input type="number" onChange={(e) => setNumberOfHours(e.target.value)} />
 			<button onClick={addCourse}>ADD COURSE</button>
+			<h1>Course List</h1>
+			{courses.map((course) => (
+				<div key={course._id} className="course">
+					<h1>{course.name}</h1>
+					<h2>{course.numberOfHours}</h2>
+				</div>
+			))}
 		</div>
 	);
 }
