4b155f31a892516b56a2b4446450894dc6f749fe
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-05-19 13:09:36 +0300

    feat: setup basic ui for adding course

--- Views/crud-app/src/App.js
+++ Views/crud-app/src/App.js
@@ -1,9 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+	const [courseName, setCourseName] = useState('');
+	const [numberOfHours, setNumberOfHours] = useState(0);
+
+	const addCourse = () => {
+		console.log(courseName, numberOfHours);
+	};
 	return (
 		<div className="App">
 			<h1>CRUD APPLICATION</h1>
+			<label>Course Name: </label>
+			<input type="text" onChange={(e) => setCourseName(e.target.value)} />
+			<label>Number of Hours: </label>
+			<input type="number" onChange={(e) => setNumberOfHours(e.target.value)} />
+			<button onClick={addCourse}>ADD COURSE</button>
 		</div>
 	);
 }
