b060db28cc4e4638717ad14d2bcf3050ca889ab0
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-05-23 14:55:51 +0300

    add url to connect to cloud database

--- index.js
+++ index.js
@@ -14,9 +14,12 @@ const PORT = 3001;
 //connect to mongoDB
 main().catch((err) => console.log(err));
 async function main() {
-	await mongoose.connect('mongodb://localhost:27017/crud', () => {
-		console.log('Connected to Database');
-	});
+	await mongoose.connect(
+		'mongodb+srv://Solomon:S0l0m0n1234@cluster0.tcc5n.mongodb.net/?retryWrites=true&w=majority',
+		() => {
+			console.log('Connected to Database');
+		}
+	);
 }
 
 app.listen(process.env.PORT || PORT, () => {
